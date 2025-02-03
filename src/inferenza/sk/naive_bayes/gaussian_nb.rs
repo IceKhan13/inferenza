4d84195800275653fb129c992db53c1f855c2ffe
javabster <abby.s.mitchell@gmail.com>
2025-02-03 16:01:43 -0500

    Added more logic to gaussian naive bayes function

--- src/inferenza/sk/naive_bayes/gaussian_nb.rs
+++ src/inferenza/sk/naive_bayes/gaussian_nb.rs
@@ -3,11 +3,14 @@ use crate::onnx;
 use crate::inferenza::sk::naive_bayes::NaiveBayesAlgorithm;
 use crate::inferenza::OnnxLoadable;
 use crate::inferenza::utils;
+use std::f64::consts::PI;
 
 // struct for GaussianNB Scikit learn model, i.e. the minimum data we need from the model to make a prediction on unseen data
 #[derive(Debug)] // Q: what does this do?
 struct GaussianNBModel {
-    test: u32,
+    mean: Array2<f32>,
+    std: Array2<f32>,
+    prior_prob: Array1<f32>,
 }
 
 
@@ -18,6 +21,7 @@ struct GaussianNBModel {
 // 2. Prediction Phase:
 // For a new data point:
 //      For each class, calculate the likelihood of observing the data point's feature values based on the Gaussian distribution for that class. 
+        // i.e. the prior probability, i.e. proportion of test data that was classified as one or other feature
 //      Multiply these likelihoods by the prior probability of each class. 
 //      Select the class with the highest resulting probability as the prediction. 
 
@@ -31,27 +35,63 @@ impl OnnxLoadable<GaussianNBModel> for GaussianNBModel
         println!("{}", model.graph); //how do i look at what the model object is??
 
         GaussianNBModel {
-            test: 123,
+            mean: [[123, 456], [234, 567]], // each row is a class, each column is a feature
+            std_deviation: [[123, 456], [234, 567]], // each row is a class, each column is a feature
+            prior_pr: [0.7, 0.3] // what proportion of trained data falls into each class
         }
     }
 }
 
 
-// impl<T> NaiveBayesAlgorithm<T> for GaussianNBModel<T>
-// where
-//     T: ndarray::LinalgScalar + Clone + std::fmt::Debug + num::Float, // what does this do again?
-// {
-//     // run inference on given input (i.e. the unseen data)
-//     fn predict(&self, input: Array2<T>) -> Array1<u32> {
-//         // vector for results
-//         let mut result: Vec<u32> = vec![];
+impl<T> NaiveBayesAlgorithm<T> for GaussianNBModel<T>
+where
+    T: ndarray::LinalgScalar + Clone + std::fmt::Debug + num::Float, // what does this do again?
+{
+    // run inference on given input (i.e. the unseen data)
+    fn predict(&self, input: Array2<T>) -> Array1<u32> {
+        // vector for results
+        let mut result: Vec<u32> = vec![];
 
-//         // iterate over every piece of unseen data to classify
-//         // for (i, row) in input.rows().into_iter().enumerate(){
-//         //     // TODO the logic for classifying a new datapoint
-//         // }
-//     }
-// }
+        // iterate over every piece of unseen data to classify
+        for (i, row) in input.rows().into_iter().enumerate(){
+            // 1. for each class and each feature in new data point, calc gaussian pdf
+            // 2. calc posterior probability for each class using the gaussian pdfs
+            // 3. final classification is the index of the largest posterior probability
+
+            let post_probs: Vec<f32> = vec![]
+
+            for (class_idx, prob) in prior_prob {
+
+                let mut class_pdf: Vec<f32> = vec![] // collect the pdf calcs for each feature for this class
+
+                for (feature_idx, feature) in row {
+                    // get the train data mean for this class and this feature
+                    let mean = self.mean[class_idx][feature_idx]
+                    // get the train data standard deviation for this class and this feature
+                    let std = self.std[class_idx][feature_idx]
+
+                    // calculate pdf for this feature
+                    let coeff = 1.0 / ((2.0 * PI * std.powi(2)).sqrt());
+                    let exponent = (-((feature - mean).powi(2)) / (2.0 * std.powi(2))).exp();
+                    let pdf = coeff * exponent
+
+                    class_pdf.push(pdf)
+                }
+
+                // calculate posterior probability for this class
+                let post_prob = self.prior_prob[class_idx] * class_pdf.iter().product()
+                post_probs.push(post_prob)
+
+            }
+
+            // the largest value in the posterior probabilities is the most likely class the new data point is
+            classification = post_probs.iter().enumerate().max_by_key(|&(_, &val)| val)
+            result.push(classification)
+        }
+
+        Array1::from_vec(result)
+    }
+}
 
 #[cfg(test)]
 mod tests {
