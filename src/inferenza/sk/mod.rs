732b6a6f5838caac577442e51b1199172aa18ee3
Iskandar Sitdikov <IceKhan13@users.noreply.github.com>
2025-02-16 14:23:13 -0500

    Supervised: LinearRegression (#4)

--- src/inferenza/sk/mod.rs
+++ src/inferenza/sk/mod.rs
@@ -1,1 +1,10 @@
 mod clustering;
+mod supervised;
+
+use ndarray::Array2;
+
+/// Trait for regression algorithms
+pub trait Regressor<T> {
+    /// Inference method for clustering algorithms
+    fn predict(&self, input: Array2<T>) -> Array2<T>;
+}
